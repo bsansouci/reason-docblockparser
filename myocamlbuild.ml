29120f552ddd2095947b023fd050928a6f368170
Alex Zelenskiy <alex.zelenskiy@cloverhealth.com>
2017-03-29 10:42:44 -0700

    removing extra stuff

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,10 +1,26 @@
+open Printf
 open Solvuu_build.Std
 
 let project_name = "DockBlock"
 let version = "dev"
 
-let mylib = Project.lib project_name
-  ~dir:"src"
-  ~style:`Basic
+let findlib_deps = [
+  "re";
+  "ounit";
+]
 
-let () = Project.basic1 ~project_name ~version [mylib]
+let lib = Project.lib project_name
+  ~dir:"src"
+  ~style:(`Pack project_name)
+  ~findlib_deps
+
+let test : Project.item = Project.app "my-app"
+  ~file:"test/test.re"
+  ~internal_deps:[lib]
+
+let ocamlinit_postfix = [
+  sprintf "open %s" (String.capitalize project_name);
+]
+
+;;
+let () = Project.basic1 ~project_name ~version [lib;test] ~ocamlinit_postfix
