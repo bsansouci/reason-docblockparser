1bc8ef2f60712be2c7c893d6d5f5b28784814361
Alex Zelenskiy <alex.zelenskiy@cloverhealth.com>
2017-03-29 17:58:17 -0700

    topkg builds

---
+++ pkg/pkg.ml
@@ -0,0 +1,19 @@
+#!/usr/bin/env ocaml
+#use "topfind"
+#require "topkg"
+open Topkg
+
+let () =
+  let cmd c os files =
+    let ocamlbuild = Conf.tool "rebuild" os in
+    OS.Cmd.run @@ Cmd.(ocamlbuild % "-use-ocamlfind"
+                                  %% (v "-I" % "src")
+                                  %% of_list files)
+  in
+  let build = Pkg.build ~cmd () in
+  Pkg.describe "dockblock" ~build ~change_logs:[] ~licenses:[] ~readmes:[] @@ fun c ->
+  Ok [
+    Pkg.lib "pkg/META";
+    Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx";".cmi"; ".cmt"; ".o"; ".cma"; ".cmxa"; ".a"]) "src/DockBlock" ~dst:"DockBlock";
+
+  ]
